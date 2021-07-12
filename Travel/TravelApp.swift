17c834077d62a2e71bb0d46e25f678d71abda55b
Milad Golchinpour <miladsoftware@icloud.com>
2021-07-12 21:39:40 +0430

    Initial Commit

---
+++ Travel/TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelApp.swift
+//  Travel
+//
+//  Created by Milad Golchinpour on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
